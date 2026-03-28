14aab4fd14046ea93b0a6ed31b06ed85e336f657
feroz315 <feroz3151@gmail.com>
2026-03-28 15:19:36 +0500

    new changes

--- frontend/src/screens/login.jsx
+++ frontend/src/screens/login.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
 import {
   View,
   Text,
@@ -10,131 +10,143 @@ import {
   Dimensions,
   SafeAreaView,
   ScrollView,
-  Linking
+  Linking,
 } from 'react-native';
-import { COLORS } from "../const/colors";
- 
+import {COLORS} from '../const/colors';
+import Toast from 'react-native-toast-message';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const { width } = Dimensions.get("screen");
-
+const {width} = Dimensions.get('screen');
 
 const Login = () => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
   const navigation = useNavigation();
 
+  const handleSubmit = async () => {
+    if (!formData.name || !formData.password) {
+      Toast.show({
+        type: 'error',
+        text1: 'Validation Error!',
+        text2: 'Login Failed!',
+      });
+    }
+    try {
+      const response = await fetch('http://192.168.1.2:3000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          password: formData.password,
+        }),
+      });
+      if (response.ok) {
+        console.log('login', response.token);
+        Toast.show({
+          type: 'success', // Type of toast: 'success', 'error', 'info'
+          text1: 'Successfully!', // Main message (header)
+          text2: 'Login get successfully!',
+        });
+        await AsyncStorage.setItem('authToken', response.token);
+        setFormData(response);
+        navigation.navigate('bottomNav');
+      }
+    } catch (e) {
+      Toast.show({
+        type: 'error',
+        text1: 'Validation Error!',
+        text2: 'Login Failed!',
+      });
+      console.log('error hai', e);
+    }
+  };
 
-  const Submit = async () => {
-   try {      
-     fetch("http://192.168.1.2:3000/api/login",{
-       method:"POST",
-       headers: {
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify({
-        "email":email,
-        "password":password,  
-      })
-    })
-     
-     .then(res=>res.json())
-     .then(async (data) => { 
-      console.log("login", data.token );
-       await AsyncStorage.setItem('authToken', data.token);
-       navigation.navigate("bottomNav")
-     })
-      } catch (e) {
-        console.log("error hai",e)
-        return { success: false, message: e.res?.data?.message || "Login failed" };
-            }
+  const ForgetPass = async () => {
+    navigation.navigate('forget');
+  };
 
-  
-  }
+  const Signup = async () => {
+    navigation.navigate('signup');
+  };
 
-  const ForgetPass = async () => {   
-    navigation.navigate("forget")
-  }
-  
-  const Signup = async () => {   
-    navigation.navigate("signup")
-  }
-  
-const openGmail = () => {
-  // Use react-native-email-link or custom linking for a better experience
-  Linking.openURL('https://accounts.google.com');
-};
-
+  const openGmail = () => {
+    // Use react-native-email-link or custom linking for a better experience
+    Linking.openURL('https://accounts.google.com');
+  };
 
   return (
- 
-   <SafeAreaView style={styles.container}>
-       <ScrollView contentContainerStyle={styles.scrollContainer}>
-         {/* App Logo/Image */}
-         <Image source={require('../assets/images/boyshop.png')} style={styles.logo}  />
- 
-         <Text style={styles.title}>Login</Text>   
-    
-         {/* Email Input */}
-         <View style={styles.inputContainer}>
-         
-           <TextInput
-             style={styles.input}
-             placeholder="Email"
-             value={email}
-             onChangeText={setEmail}
-             keyboardType="email-address"
-             autoCapitalize="none"
-           />
-         </View>
- 
-         {/* Password Input */}
-         <View style={styles.inputContainer}>
-           <TextInput
-             style={styles.input}
-             placeholder="Password"
-             value={password}
-             onChangeText={setPassword}
-             secureTextEntry
-           />
-         </View>
- 
-         
-         {/* Forget Button */}
-         <TouchableOpacity onPress={ForgetPass}>
-           <Text style={styles.forgetButtonText}>Forgot Password? </Text>
-         </TouchableOpacity>
- 
-         {/* Signup Button */}
-         <TouchableOpacity style={styles.signupButton} onPress={Submit}>
-           <Text style={styles.signupButtonText}>Login </Text>
-         </TouchableOpacity>
-   
-     <View style={styles.accountsign}>
-      <Text style={styles.forgetButtonText}>Don't have an account? </Text>
-          <TouchableOpacity onPress={Signup}>    
-           <Text style={styles.signupbtn}>Sign Up </Text>
-         </TouchableOpacity>
-     </View>
-   
-       <Text style={styles.forgetButtonText}>Or </Text>
-   
-    {/* Gmail Button */}
-     <View style={styles.gmailbtn}>
-          <TouchableOpacity style={styles.GmailButton} onPress={openGmail}>
-          <Image source={require('../assets/images/gmail.jpg')} style={styles.gmaillogo} />
-           <Text style={styles.gmailtext}>Sign in With Google </Text>
-         </TouchableOpacity>
-     </View>
-   
-       </ScrollView>
-     </SafeAreaView>
-     
-     );
-}
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        {/* App Logo/Image */}
+        <Image
+          source={require('../assets/images/boyshop.png')}
+          style={styles.logo}
+        />
 
+        <Text style={styles.title}>Login</Text>
+
+        {/* Email Input */}
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={formData.email}
+            onChangeText={text => setFormData({...formData, email: text})}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
+
+        {/* Password Input */}
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={formData.password}
+            onChangeText={text => setFormData({...formData, password: text})}
+            secureTextEntry
+          />
+        </View>
+
+        {/* Forget Button */}
+        <TouchableOpacity onPress={ForgetPass}>
+          <Text style={styles.forgetButtonText}>Forgot Password? </Text>
+        </TouchableOpacity>
+
+        {/* Signup Button */}
+        <TouchableOpacity style={styles.signupButton} onPress={handleSubmit}>
+          <Text style={styles.signupButtonText}>Login </Text>
+        </TouchableOpacity>
+
+        <View style={styles.accountsign}>
+          <Text style={styles.forgetButtonText}>Don't have an account? </Text>
+          <TouchableOpacity onPress={Signup}>
+            <Text style={styles.signupbtn}>Sign Up </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.forgetButtonText}>Or </Text>
+
+        {/* Gmail Button */}
+        <View style={styles.gmailbtn}>
+          <TouchableOpacity style={styles.GmailButton} onPress={openGmail}>
+            <Image
+              source={require('../assets/images/gmail.jpg')}
+              style={styles.gmaillogo}
+            />
+            <Text style={styles.gmailtext}>Sign in With Google </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -153,11 +165,11 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     alignSelf: 'center',
   },
- 
+
   title: {
     fontSize: 28,
     fontWeight: '500',
-    fontFamily:"Poppins",
+    fontFamily: 'Poppins',
     color: '#333',
     marginBottom: 25,
     textAlign: 'center',
@@ -171,8 +183,6 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: '100%',
     marginBottom: 15,
-    
-    
   },
   input: {
     borderWidth: 1,
@@ -182,32 +192,29 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     fontSize: 16,
     backgroundColor: '#f9f9f9',
-   
   },
   signupButton: {
-    // backgroundColor: '#FF6B35', 
+    // backgroundColor: '#FF6B35',
     width: width * 0.75,
-    height:45,
+    height: 45,
     backgroundColor: COLORS.navy,
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 20,
-    marginTop:50,
-     
+    marginTop: 50,
   },
   signupButtonText: {
-    fontSize:16,
+    fontSize: 16,
     color: COLORS.white,
-    textAlign:'center'      
- 
+    textAlign: 'center',
   },
   forgetButtonText: {
-    fontSize:16,
+    fontSize: 16,
     color: COLORS.navy,
-    textAlign:'center'      
-   },
-  
+    textAlign: 'center',
+  },
+
   loginText: {
     marginTop: 20,
     fontSize: 14,
@@ -218,52 +225,47 @@ const styles = StyleSheet.create({
     color: '#FF6B35',
     fontWeight: 'bold',
   },
-  accountsign:{
-    flexDirection:"row",
-    alignItems:'center',
-    justifyContent:'center',
-    marginVertical:20
+  accountsign: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 20,
   },
-  signupbtn:{
-    fontSize:15,
-    fontWeight:"700",
+  signupbtn: {
+    fontSize: 15,
+    fontWeight: '700',
     color: COLORS.navy,
-    textAlign:'center',
-
+    textAlign: 'center',
   },
   GmailButton: {
-    // backgroundColor: '#FF6B35', 
+    // backgroundColor: '#FF6B35',
     width: width * 0.75,
-    height:45,
+    height: 45,
     // backgroundColor: COLORS.navy,
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 15,
-    marginTop:30,
-    borderWidth:2,
-    borderColor:"#000"
-     
+    marginTop: 30,
+    borderWidth: 2,
+    borderColor: '#000',
   },
- gmailtext:{
-    fontSize:16,
+  gmailtext: {
+    fontSize: 16,
     color: COLORS.dark,
-    textAlign:'center',
-    fontFamily:"Poppins",
-         
+    textAlign: 'center',
+    fontFamily: 'Poppins',
   },
- gmaillogo: {
+  gmaillogo: {
     width: 30,
     height: 30,
     alignSelf: 'center',
   },
-  gmailbtn:{
-   flexDirection:'row',
-   alignItems:"center",
-   justifyContent:'center',
-   
+  gmailbtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
 
 export default Login;
--- frontend/src/screens/signup.jsx
+++ frontend/src/screens/signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -11,93 +11,89 @@ import {
   Alert,
   ScrollView,
 } from 'react-native';
-import { COLORS } from "../const/colors";
+import {COLORS} from '../const/colors';
 import Toast from 'react-native-toast-message';
- 
 
-const { width } = Dimensions.get("screen");
+const {width} = Dimensions.get('screen');
 
-
-import { useNavigation } from '@react-navigation/native';
-
+import {useNavigation} from '@react-navigation/native';
 
 const Signup = () => {
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
   const navigation = useNavigation();
 
-  
-
-  const Submit = async () => {
-    if(!name || !email || !password) {
+  const handleSubmit = async () => {
+    if (!formData.name || !formData.email || !formData.password) {
       Toast.show({
-       type: 'error',
-       text1: 'Validation Error!',
-       text2: 'User Failed!'
-     });     
+        type: 'error',
+        text1: 'Validation Error!',
+        text2: 'Register Failed!',
+      });
     }
-   try {      
-     fetch("http://192.168.1.2:3000/api/register",{
-       method:"POST",
-       headers: {
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify({
-        "email":email,
-        "name":name,
-        "password":password, 
-      })
-      
-     })
-     .then(res=>res.json())
-     .then(async (data)=> {
+    try {
+      const response = await fetch('http://192.168.1.2:3000/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          name: formData.name,
+          password: formData.password,
+        }),
+      });
+      if (response.ok) {
+        console.log('Success', response.ok);
+        Toast.show({
+          type: 'success', // Type of toast: 'success', 'error', 'info'
+          text1: 'Successfully!', // Main message (header)
+          text2: 'Register get successfully!',
+        });
+        setFormData(response);
+        navigation.navigate('login');
+      }
+    } catch (e) {
       Toast.show({
-      type: 'success', // Type of toast: 'success', 'error', 'info'
-      text1: 'Successfully!',  // Main message (header)
-      text2: 'Register get successfully!' 
-  });      
-       console.log(data)
-       navigation.navigate("login")       
-     })
-      } catch (e) {
-       Toast.show({
-       type: 'error',
-       text1: 'Validation Error!',
-       text2: 'Failed!'
-     });     
+        type: 'error',
+        text1: 'Validation Error!',
+        text2: 'Failed!',
+      });
 
-        console.log("error hai",e)
+      console.log('error hai', e);
     }
-  }
-
-  const openGmail = () => {
-  // Use react-native-email-link or custom linking for a better experience
-  Linking.openURL('https://accounts.google.com');
   };
 
- 
-  const Login = async () => {   
-    navigation.navigate("login")
-  }
+  const openGmail = () => {
+    // Use react-native-email-link or custom linking for a better experience
+    Linking.openURL('https://accounts.google.com');
+  };
+
+  const Login = async () => {
+    navigation.navigate('login');
+  };
 
   return (
-
-  <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         {/* App Logo/Image */}
-        <Image source={require('../assets/images/boy.png')} style={styles.logo} />
+        <Image
+          source={require('../assets/images/boy.png')}
+          style={styles.logo}
+        />
         <Text style={styles.title}>Sign Up </Text>
-       
+
         {/* Full Name Input */}
         <View style={styles.inputContainer}>
           <TextInput
             style={styles.input}
             placeholder="Name"
-            value={name}
-            onChangeText={setName}
+            value={formData.name}
+            onChangeText={text => setFormData({...formData, name: text})}
             autoCapitalize="words"
           />
         </View>
@@ -107,8 +103,8 @@ const Signup = () => {
           <TextInput
             style={styles.input}
             placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
+            value={formData.email}
+            onChangeText={text => setFormData({...formData, email: text})}
             keyboardType="email-address"
             autoCapitalize="none"
           />
@@ -119,42 +115,40 @@ const Signup = () => {
           <TextInput
             style={styles.input}
             placeholder="Password"
-            value={password}
-            onChangeText={setPassword}
+            value={formData.password}
+            onChangeText={text => setFormData({...formData, password: text})}
             secureTextEntry
           />
         </View>
 
-        
         {/* Signup Button */}
-        <TouchableOpacity style={styles.signupButton} onPress={Submit}>
+        <TouchableOpacity style={styles.signupButton} onPress={handleSubmit}>
           <Text style={styles.signupButtonText}>Register</Text>
         </TouchableOpacity>
 
-   <View style={styles.accountsign}>
-      <Text style={styles.forgetButtonText}>Don't have an account? </Text>
-          <TouchableOpacity onPress={Login}>    
-           <Text style={styles.signupbtn}>Login</Text>
-         </TouchableOpacity>
-     </View>
-   
-       <Text style={styles.forgetButtonText}>Or </Text>
-   
-    {/* Gmail Button */}
-     <View style={styles.gmailbtn}>
-          <TouchableOpacity style={styles.GmailButton} onPress={openGmail}>
-          <Image source={require('../assets/images/gmail.jpg')} style={styles.gmaillogo} />
-           <Text style={styles.gmailtext}>Sign in With Google </Text>
-         </TouchableOpacity>
-     </View>
-   
-    </ScrollView>
-    </SafeAreaView>
+        <View style={styles.accountsign}>
+          <Text style={styles.forgetButtonText}>Don't have an account? </Text>
+          <TouchableOpacity onPress={Login}>
+            <Text style={styles.signupbtn}>Login</Text>
+          </TouchableOpacity>
+        </View>
 
+        <Text style={styles.forgetButtonText}>Or </Text>
+
+        {/* Gmail Button */}
+        <View style={styles.gmailbtn}>
+          <TouchableOpacity style={styles.GmailButton} onPress={openGmail}>
+            <Image
+              source={require('../assets/images/gmail.jpg')}
+              style={styles.gmaillogo}
+            />
+            <Text style={styles.gmailtext}>Sign in With Google </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -173,16 +167,14 @@ const styles = StyleSheet.create({
     // backgroundColor:"#000",
     marginBottom: 5,
     alignSelf: 'center',
-    
   },
   title: {
     fontSize: 28,
     fontWeight: '500',
-    fontFamily:"Poppins",
+    fontFamily: 'Poppins',
     color: '#333',
     marginBottom: 25,
     textAlign: 'center',
-  
   },
   subtitle: {
     fontSize: 16,
@@ -204,23 +196,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#f9f9f9',
   },
   signupButton: {
-      // backgroundColor: '#FF6B35', 
-      width: width * 0.75,
-      height:45,
-      backgroundColor: COLORS.navy,
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: 'center',
-      borderRadius: 20,
-      marginTop:30,
-       
-    },
-    signupButtonText: {
-      fontSize:16,
-      color: COLORS.white,
-      textAlign:'center'      
-   
-    },
+    // backgroundColor: '#FF6B35',
+    width: width * 0.75,
+    height: 45,
+    backgroundColor: COLORS.navy,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    marginTop: 30,
+  },
+  signupButtonText: {
+    fontSize: 16,
+    color: COLORS.white,
+    textAlign: 'center',
+  },
   loginText: {
     marginTop: 20,
     fontSize: 14,
@@ -231,52 +221,47 @@ const styles = StyleSheet.create({
     color: '#FF6B35',
     fontWeight: 'bold',
   },
-  accountsign:{
-    flexDirection:"row",
-    alignItems:'center',
-    justifyContent:'center',
-    marginVertical:20
+  accountsign: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 20,
   },
-  signupbtn:{
-    fontSize:15,
-    fontWeight:"700",
+  signupbtn: {
+    fontSize: 15,
+    fontWeight: '700',
     color: COLORS.navy,
-    textAlign:'center'      
+    textAlign: 'center',
   },
   GmailButton: {
-    // backgroundColor: '#FF6B35', 
+    // backgroundColor: '#FF6B35',
     width: width * 0.75,
-    height:45,
+    height: 45,
     // backgroundColor: COLORS.navy,
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 15,
-    marginTop:30,
-    borderWidth:2,
-    borderColor:"#000"
-     
+    marginTop: 30,
+    borderWidth: 2,
+    borderColor: '#000',
   },
- gmailtext:{
-    fontSize:16,
+  gmailtext: {
+    fontSize: 16,
     color: COLORS.dark,
-    textAlign:'center',
-    fontFamily:"Poppins",
-         
+    textAlign: 'center',
+    fontFamily: 'Poppins',
   },
- gmaillogo: {
+  gmaillogo: {
     width: 30,
     height: 30,
     alignSelf: 'center',
   },
-  gmailbtn:{
-   flexDirection:'row',
-   alignItems:"center",
-   justifyContent:'center',
-   
+  gmailbtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-
 
 export default Signup;
