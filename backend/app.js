dfe697c93b16bc5f835a83e08baea888c977e2ec
feroz315 <feroz3151@gmail.com>
2026-06-13 13:10:54 +0500

    update

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,12 @@ const router = require('./routes/authRoutes.js');
 const app = express();
 
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // const dotenv = require("dotenv");
 // dotenv.config();
-
-
 // const Port = process.env.PORT || 3000;
 
 
