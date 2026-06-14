cb540149045587a29da1df8d504b0d6b5c3c8c8e
feroz315 <feroz3151@gmail.com>
2026-06-14 11:27:49 +0500

    new

--- frontend/android/app/src/main/java/com/frontend/myapp/MainActivity.kt
+++ frontend/android/app/src/main/java/com/frontend/myapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.frontend
+package com.frontend.myapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- frontend/android/app/src/main/java/com/frontend/myapp/MainApplication.kt
+++ frontend/android/app/src/main/java/com/frontend/myapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.frontend
+package com.frontend.myapp
 
 import android.app.Application
 import com.facebook.react.PackageList
